09a08b7e52e86c6a5c7d6c836374cb170c6d4605
Roland Brannon <roland.brannon.am@gmail.com>
2022-08-20 16:31:40 +0300

    init print_anime and print_season function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 #define BIT(x) (1 << (x))
 
@@ -17,10 +18,31 @@ enum genres
 	paranormal = BIT(6),
 	drama = BIT(7),
 	school = BIT(8),
-	isekai = BIT(9),
+	ISEKAI = BIT(9),
 	harem = BIT(10),
 	casual = BIT(11),
 };
+
+void print_if(bitmask genres, bitmask mask, const string& text)
+{
+	if (genres & mask) std::cout << " - " << text << std::endl;
+}
+
+void print_genres(bitmask genres)
+{
+	print_if(genres, shounen, "shounen");
+	print_if(genres, romantic, "romantic");
+	print_if(genres, action, "action");
+	print_if(genres, ecchi, "ecchi");
+	print_if(genres, comedy, "comedy");
+	print_if(genres, shoujo, "shoujo");
+	print_if(genres, paranormal, "paranormal");
+	print_if(genres, drama, "drama");
+	print_if(genres, school, "school");
+	print_if(genres, ISEKAI, "ISEKAI");
+	print_if(genres, harem, "harem");
+	print_if(genres, casual, "casual");
+}
 
 enum class time_season
 {
@@ -31,6 +53,17 @@ enum class time_season
 	fall = autumn,
 };
 
+void print_season(time_season season)
+{
+	switch (season)
+	{
+	case time_season::winter:std::cout << "Winter" << std::endl; break;
+	case time_season::spring:std::cout << "spring" << std::endl; break;
+	case time_season::summer:std::cout << "summer" << std::endl; break;
+	case time_season::fall:std::cout << "fall" << std::endl;     break;
+	}
+}
+
 enum class status
 {
 	ongoing,
@@ -39,28 +72,91 @@ enum class status
 	anounce,
 };
 
+void print_status(status status)
+{
+	switch (status)
+	{
+	case status::ongoing:std::cout << "ongoing" << std::endl; break;
+	case status::release:std::cout << "release" << std::endl; break;
+	case status::frozen:std::cout << "frozen" << std::endl;   break;
+	case status::anounce:std::cout << "anounce" << std::endl; break;
+	}
+}
+
 struct season
 {
+	season() = default;
+	season(const string& new_name, time_season new_time, int new_episodes, status new_status)
+	{
+		name = new_name;
+		time = new_time;
+		episodes = new_episodes;
+		status = new_status;
+	}
+	
 	string name = "some shit";
 	time_season time = time_season::fall;
 	int episodes = 0;
 	status status = status::ongoing;
+
+	void print() const
+	{
+		std::cout << " - Name: " << name
+		<< "\n - Episodes: " << episodes << std::endl;
+		std::cout << " - Season: ";
+		print_season(time);
+		std::cout << " - Status: ";
+		print_status(status);
+	}
 };
 
 struct anime
 {
+	anime() = default;
+	anime(const string& new_name, bitmask new_genres, const string& new_author)
+	{
+		name = new_name;
+		genres = new_genres;
+		author = new_author;
+	}
+	
 	string name = "some shit";
 	bitmask genres = 0;
 	string author = "Tsugumi Oba";
 	
-	int episodes = 0;
 	string studio = "some studio";
 	std::vector<season> seasons;
+
+	void print() const
+	{
+		std::cout << "Name: " << name
+		          << "\nAuthor: " << author 
+		          << "\nStudio: " << studio << std::endl;
+		print_genres(genres);
+		for (int i = 0; i < seasons.size(); i++)
+		{
+			std::cout << "Season " << i+1 << std::endl;
+			seasons[i].print();
+		}
+	}
+
+	void add_season(const string& new_name, time_season new_time, int new_episodes, status new_status)
+	{
+		season season(new_name, new_time, new_episodes, new_status);
+		seasons.push_back(season);
+	}
 };
 
 
 int main()
 {
-	unsigned int genre = action | comedy;
+	anime Naruto("Naruto", shounen | comedy | drama | action, "Masashi Kishimoto");
+	Naruto.studio = "Pierrot";
+	
+	Naruto.add_season("Naruto", time_season::fall, 220, status::release);
+	Naruto.add_season("Naruto Shippuden", time_season::winter, 500, status::release);
+
+	Naruto.print();
+
 	return 0;
 }
